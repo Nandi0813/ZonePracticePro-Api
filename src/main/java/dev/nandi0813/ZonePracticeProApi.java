eec3f5d643f729e1d7e7899fc2644a592b51e918
Dukát Nándor <dukatnandor@gmail.com>
2024-08-06 16:05:36 +0200

    1.5.0

--- src/main/java/dev/nandi0813/ZonePracticeProApi.java
+++ src/main/java/dev/nandi0813/ZonePracticeProApi.java
@@ -5,6 +5,8 @@ import org.bukkit.entity.Player;
 public interface ZonePracticeProApi
 {
 
+    ZonePracticeProApi getInstance();
+
     enum DivisionName {
         SHORT,
         FULL
@@ -19,6 +21,8 @@ public interface ZonePracticeProApi
     void resetPlayerUnRanked(Player player);
 
     void resetPlayerRanked(Player player);
+
+    void addPlayerUnRanked(Player player, int amount);
 
     void addPlayerRanked(Player player, int amount);
 
